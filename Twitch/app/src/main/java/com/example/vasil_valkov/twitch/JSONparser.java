8ec3281e575777fbe7378f63e5d584b0ab7d4cce
vasilvalkov123 <vasilvalkov123@gmail.com>
2015-03-26 15:41:21 +0200

    Twitch
    Show game name

---
+++ Twitch/app/src/main/java/com/example/vasil_valkov/twitch/JSONparser.java
@@ -0,0 +1,83 @@
+package com.example.vasil_valkov.twitch;
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by Vasil-Valkov on 26.3.2015 г..
+ */
+public class JSONparser {
+
+    static InputStream is = null;
+    static JSONObject jObj = null;
+    static String json = "";
+
+    // constructor
+    public JSONparser() {
+
+    }
+
+    public JSONObject getJSONFromUrl(String url) {
+
+        // Making HTTP request
+        try {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+
+            HttpUriRequest request = new HttpGet(url);
+            request.setHeader("Accept", "application/json");
+
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity httpEntity = response.getEntity();
+
+            is = httpEntity.getContent();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+            jObj = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jObj;
+
+    }
+}
---
+++ Twitch/app/src/main/java/com/example/vasil_valkov/twitch/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.vasil_valkov.twitch;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        TextView game,viewers;
+
+        private String url = "https://api.twitch.tv/kraken/streams/stormstudio_d2cl_ru";
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            game = (TextView) rootView.findViewById(R.id.game_name);
+            viewers = (TextView) rootView.findViewById(R.id.game_viewers);
+
+            new JSONParse().execute();
+
+            return rootView;
+        }
+
+
+        private class JSONParse extends AsyncTask<String, String, JSONObject> {
+
+            @Override
+            protected JSONObject doInBackground(String... params) {
+                JSONparser parse = new JSONparser();
+
+                JSONObject json = parse.getJSONFromUrl(url);
+                return json;
+            }
+
+            @Override
+            protected void onPostExecute(JSONObject json) {
+
+                JSONObject object = null;
+                try {
+                    object = json.getJSONObject("stream");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                assert object != null;
+                String game_name = null;
+                String  game_viewers = null;
+                try {
+                    game_name = object.getString("game");
+                    game_viewers = object.getString("viewers");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                game.setText(game_name);
+                viewers.setText(game_viewers);
+
+            }
+        }
+    }
+}
